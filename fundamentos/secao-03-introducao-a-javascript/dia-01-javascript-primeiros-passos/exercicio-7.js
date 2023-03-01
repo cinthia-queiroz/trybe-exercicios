5438986964706d9e34e2e237d40ec0371e7c8d44
Cinthia Queiroz <queiroz.cinthia@outlook.com>
2023-03-01 13:01:47 -0300

    Exercício 7

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-7.js
@@ -0,0 +1,17 @@
+let grade = 40;
+
+if (grade < 0 || grade > 100) {
+    console.log('nota inválida!')
+} else if (grade >= 90) {
+    console.log('A');
+} else if (grade >= 80) {
+    console.log('B');
+} else if (grade >= 70) {
+    console.log('C');
+} else if (grade >= 60) {
+    console.log('D');
+} else if (grade >= 50) {
+    console.log('E');
+} else {
+    console.log('F');
+}
