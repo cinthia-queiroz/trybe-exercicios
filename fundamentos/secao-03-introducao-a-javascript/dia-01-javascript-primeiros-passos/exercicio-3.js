80cf94d296037c8b2c13128d1b396ef3d7203b9a
Cinthia Queiroz <queiroz.cinthia@outlook.com>
2023-03-01 12:05:36 -0300

    Exercício 3

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-3.js
@@ -0,0 +1,11 @@
+let a = 7;
+let b = 2;
+let c = 5;
+
+if (a > b && a > c) {
+    console.log('a é o maior número');
+} else if (b > a && b > c) {
+    console.log('b é o maior número');
+} else {
+    console.log('c é o maior número');
+}
